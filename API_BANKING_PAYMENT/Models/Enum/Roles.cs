106744eae329a4e061ca57ffb1aaa67dd234590a
hanzala.sarguroh@aurionpro.com <hanzala.sarguroh@aurionpro.com>
2025-09-19 15:45:33 +0530

    added roles

---
+++ API_BANKING_PAYMENT/Models/Enum/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace API_BANKING_PAYMENT.Models.Enum
+{
+    public static class Roles
+    {
+        public const string SuperAdmin = "SuperAdmin";
+        public const string BankUser = "BankUser";
+        public const string ClientUser = "ClientUser";
+    }
+
+}
