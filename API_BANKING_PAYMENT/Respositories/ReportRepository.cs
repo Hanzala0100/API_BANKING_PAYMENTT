8791e7f68166ddbaaf37a345c2c9052d291c3ede
hanzala.sarguroh@aurionpro.com <hanzala.sarguroh@aurionpro.com>
2025-09-19 16:19:33 +0530

    Refactor repository files to improve structure and organization

--- API_BANKING_PAYMENT/Respositories/ReportRepository.cs
+++ API_BANKING_PAYMENT/Respositories/ReportRepository.cs
@@ -1,6 +1,8 @@
 ﻿using API_BANKING_PAYMENT.Models;
 using API_BANKING_PAYMENT.Respositories.IRepositories;
 
+using API_BANKING_PAYMENT.Models.Entities;
+
 namespace API_BANKING_PAYMENT.Respositories
 {
     public class ReportRepository : Repository<Report>, IReportRepository
